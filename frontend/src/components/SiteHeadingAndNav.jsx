07c52c72c04d7f4a8bd67864aa15955e461b29a3
Kevin <mrkev63@gmail.com>
2024-10-11 21:14:14 -0400

    did some tweaks to nav bar

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -6,12 +6,12 @@ export default function SiteHeadingAndNav() {
   const { currentUser } = useContext(CurrentUserContext);
 
   return <header>
-    <a id='logo' href='/'>React/Express Auth</a>
     <nav>
       <ul>
         <li><NavLink to='/'>Home</NavLink></li>
+        <li></li>
 
-        {
+        {/* {
           currentUser
             ? <>
               <li><NavLink to='/users' end={true}>Users</NavLink></li>
@@ -21,7 +21,7 @@ export default function SiteHeadingAndNav() {
               <li><NavLink to='/login'>Login</NavLink></li>
               <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
             </>
-        }
+        } */}
       </ul>
     </nav>
   </header>;
