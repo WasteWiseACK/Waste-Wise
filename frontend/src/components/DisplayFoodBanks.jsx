df211924b6b786fec8dbd16440cb86a0a27bd327
Cindy Nguyen <cnguyen5138@gmail.com>
2024-10-12 20:56:00 -0400

    working on foodbanks page

---
+++ frontend/src/components/DisplayFoodBanks.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, useContext } from "react";
+import { fetchHandler } from "../utils/fetchingUtils";
+import FilteredFoodBanksContext from "../contexts/filtered-foodbanks-context";
+
+function DisplayFoodBanks() {
+    const [initialFoodBanks, setInitialFoodBanks] = useState([]);
+    const { setError, filteredFoodBanks } = useContext(FilteredFoodBanksContext);
+    const fetchInitialBanks = async () => {
+        const [data, error] = await fetchHandler('https://data.cityofnewyork.us/resource/if26-z6xq.json');
+        const banks = [];
+        for (let i = 0; i < data.length && banks.length <= 25; i++) {
+            let bank = data[Math.floor(Math.random() * (data.length))];
+            banks.push(bank);
+        }
+        if (data) {
+            setInitialFoodBanks(banks)
+        } if (error) {
+            setError(error)
+        }
+    }
+    useEffect(() => {
+        fetchInitialBanks();
+    }, [])
+    return (
+        <div>
+            <ul>
+                {(filteredFoodBanks ? filteredFoodBanks : initialFoodBanks).map((bank) => {
+                    <li key={bank.ct2010}>
+                        <div class="bank-container">
+                            <h3>{bank.food_scrap_drop_off_site}</h3>
+                            <p>{bank.location}</p>
+                            <p>{bank.open_months}</p>
+                            <p>{bank.operation_day_hours}</p>
+                            <p>{bank.website}</p>
+                        </div>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default DisplayFoodBanks;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import UserContextProvider from './contexts/CurrentUserContextProvider.jsx';
 import './styles/index.css';
+import FilteredFoodBanksContextProvider from './contexts/FilteredFoodBanksContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FilteredFoodBanksContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FilteredFoodBanksContextProvider>
   </UserContextProvider>,
 );
