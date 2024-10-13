b1027b218eff3aa7b6ff53490c5fd49fbcfc1a24
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-10-13 10:26:08 -0400

    Added first solution

--- frontend/src/pages/Solutions.jsx
+++ frontend/src/pages/Solutions.jsx
@@ -3,8 +3,18 @@
 const Solutions = () => {
   return (
     <>
-      <h1>HELLO THIS IS SOLUTIONS PAGE</h1>
-      <h2>Stop looking, I'm not done</h2>
+      <h1>SOLUTIONS PAGE</h1>
+      <h2>There are several solutions we can do to support the battle against food-waste; Here are a few best solutions :</h2>
+
+      <h3>Solution One</h3>
+      <p>
+        Composting is a useful method for handling food-waste because it produces nutrient-rich compost that can improve soil from food scraps, yard trash, and other biodegradable items.
+        Establishing community composting programs in residences, workplaces, and educational institutions might motivate people to contribute to the reduction of food waste.
+        In addition to reducing the amount of trash dumped in landfills, composting also reduces greenhouse gas emissions from food decay.
+        Additionally, the compost that is produced can be applied to farms, gardens, and landscaping projects, which will support sustainable agricultural methods and create healthier plants.
+      </p>
+
+
     </>
   )
 }
