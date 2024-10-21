fe745b69c3a3f3d39182bda14d2415151eb7672a
Kevin <mrkev63@gmail.com>
2024-10-21 19:55:50 -0400

    Solution page (mostly) done, Food Banks page done

--- frontend/src/components/FilterFoodBanks.jsx
+++ frontend/src/components/FilterFoodBanks.jsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from "react";
 import { fetchHandler } from "../utils/fetchingUtils";
 import FilteredFoodBanksContext from "../contexts/filtered-foodbanks-context";
+import { MotionConfig, motion } from "framer-motion";
+
 function FilterFoodBanks() {
     const boroughs = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'Staten Island'];
     const [selectedBoro, setSelectedBoro] = useState('');
@@ -22,13 +24,45 @@ function FilterFoodBanks() {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="boroughs"></label>
-                <select name="boroughs" id="boroughs" onChange={handleSelectedBoro}>
-                    {boroughs.map((borough) => (
-                        <option value={borough} key={borough}>{borough}</option>
-                    ))}
-                </select>
-                <button>Filter by borough</button>
+                <legend className="body" id="legend_borough">Select Your Borough</legend>
+                <MotionConfig
+                    transition={{
+                        duration: "0.25",
+                        ease: "easeInOut"
+                    }}
+                >
+                    <motion.select
+                        className="body"
+                        name="boroughs"
+                        id="boroughs"
+                        onChange={handleSelectedBoro}
+                        whileHover={{ scale: 1.02, backgroundColor: "#6b8a7a", color: "#fefae0", cursor: "pointer" }}
+                    >
+                        {boroughs.map((borough) => (
+                            <option value={borough} key={borough}>{borough}</option>
+                        ))}
+                    </motion.select>
+                </MotionConfig>
+
+                <div className="button_filter">
+                    <MotionConfig
+                        transition={{
+                            duration: "0.25",
+                            ease: "easeInOut"
+                        }}
+                    >
+                        <motion.button
+                            className="body"
+                            id="filtering"
+                            whileHover={{ scale: 1.05, backgroundColor: "#6b8a7a", color: "#fefae0", cursor: "pointer" }}
+                            whileTap={{ scale: 0.95, rotate: '3deg' }}
+                        >
+                            Filter by borough
+                        </motion.button>
+                    </MotionConfig>
+
+                </div>
+
             </form>
         </div>
     )
--- frontend/src/pages/FoodBanks.jsx
+++ frontend/src/pages/FoodBanks.jsx
@@ -4,17 +4,41 @@ import Footer from "../components/Footer";
 
 function FoodBank() {
     return (
-        <div className="container">
-            <>
-                <main>
-                    <div className="food_bank_container">
-                        <FilterFoodBanks />
-                        <DisplayFoodBanks />
-                    </div>
+        <div>
 
-                </main>
-            </>
+
+            <main>
+                <div className="container">
+                    <section className="title_and_display">
+                        <div className="food_bank_title_container">
+                            <h1 className="header2">
+                                FOOD BANKS
+                            </h1>
+                            <h1 className="header2">
+                                FOOD BANKS
+                            </h1>
+                            <h1 className="header2">
+                                FOOD BANKS
+                            </h1>
+                            <h1 className="header2">
+                                FOOD BANKS
+                            </h1>
+                            <h1 className="header2">
+                                FOOD BANKS
+                            </h1>
+                            {/* <span id="solution_underline" className="underline" /> */}
+                            <p>waste wise</p>
+                        </div>
+                        <div className="food_bank_container">
+                            <FilterFoodBanks />
+                            <DisplayFoodBanks />
+                        </div>
+                    </section>
+                </div>
+            </main>
+            <Footer />
         </div>
+
 
     )
 };
