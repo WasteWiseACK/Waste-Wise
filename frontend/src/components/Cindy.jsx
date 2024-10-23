ad1b58b82327d5a3b81fb1d93b158e2840a77364
Kevin <mrkev63@gmail.com>
2024-10-23 18:54:25 -0400

    Forum page formatted

--- frontend/src/components/Cindy.jsx
+++ frontend/src/components/Cindy.jsx
@@ -2,15 +2,20 @@ import squidgame from '../assets/squidgame.jpg'
 
 function Cindy() {
     return (
-        <>
-            <h1>Cindy Nguyen</h1>
-            <h2>Scrum Master and Developer</h2>
-            <img src={squidgame} alt="Cindy with squidgame militia" height={500} />
-            <p>She/Hers/Her</p>
-            <p>For WasteWise, she served as the scrum master through faciliating sprint planning and daily stand-ups.</p>
-            <p>In addition to this, Cindy also actively contributed to both the frontend and backend, assisting with key development tasks.</p>
-            <a href="https://www.linkedin.com/in/sindnguyen/">Her LinkedIn</a>
-        </>
+        <div className='its_us'>
+            <div className='img_container_about'>
+                <img src={squidgame} alt="Cindy with squidgame militia" height={300} />
+            </div>
+            <div className='description_about'>
+                <h1>Cindy Nguyen</h1>
+                <caption className='body' id='pronouns'>She/Hers/Her</caption>
+                <p>For WasteWise, she served as the scrum master through faciliating sprint planning and daily stand-ups.</p>
+                <p>In addition to this, Cindy also actively contributed to both the frontend and backend, assisting with key development tasks.</p>
+                <a href="https://www.linkedin.com/in/sindnguyen/">Her LinkedIn</a>
+                <h1 className='header2' id='role'>Scrum Master and Developer</h1>
+            </div>
+
+        </div>
     )
 }
 
--- frontend/src/components/Kevin.jsx
+++ frontend/src/components/Kevin.jsx
@@ -2,16 +2,22 @@ import kevinWithFrog from '../assets/kevinWithFrog.jpg';
 
 function Kevin() {
     return (
-        <>
-            <h1>Kevin Deleon</h1>
-            <h2>Frontend Developer</h2>
-            <img src={kevinWithFrog} alt="Kevin with a frog" height={500} />
-            <p>He/Him/His</p>
-            <p>For WasteWise, he played a large role in leading the styling and design of the application.</p>
-            <p> Kevin also played
-                a key role in connecting frontend components to the backend, facilitating smooth data flow and integration between the client and server.</p>
-            <a href="https://www.linkedin.com/in/kevin-deleon-55047a26a/">His LinkedIn</a>
-        </>
+        <div className='its_us'>
+            <div className='img_container_about'>
+                <img src={kevinWithFrog} alt="Kevin with a frog" height={300} />
+            </div>
+
+            <div className='description_about'>
+                <h1>Kevin Deleon</h1>
+                <caption className='body' id='pronouns'>He/Him/His</caption>
+                <p>For WasteWise, he played a large role in leading the styling and design of the application.</p>
+                <p> Kevin also played
+                    a key role in connecting frontend components to the backend, facilitating smooth data flow and integration between the client and server.</p>
+                <a href="https://www.linkedin.com/in/kevin-deleon-55047a26a/">His LinkedIn</a>
+                <h1 className='header2' id='role'>Frontend Developer</h1>
+            </div>
+
+        </div>
     )
 }
 
