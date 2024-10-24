c9d48d1e919872f882e447bb3eef92a690894aea
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-10-23 23:12:30 -0400

    Added in heat map functionality in comments as I build the rebuild the backend

--- frontend/src/components/FoodInsecurityMap.jsx
+++ frontend/src/components/FoodInsecurityMap.jsx
@@ -1,17 +1,37 @@
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer } from "react-leaflet";
 
 function FoodInsecurity() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch('/api/heatmap-data');
+  //     const result = await response.json();
+  //     setData(result);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
-    <div className='FoodInsecurity'>
-      <MapContainer center={[40.730610, -73.935242]} zoom={11.6}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
+    <div className='FoodInsecurityMap'>
+      <MapContainer center={[40.730610, -73.935242]} zoom={11.6} >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <HeatmapLayer
+        points={data.map(point => ({
+          lat: point.latitude,
+          lng: point.longitude,
+          value: point.intensity,
+        }))}
+        longitudeExtractor={m => m.lng}
+        latitudeExtractor={m => m.lat}
+        valueExtractor={m => m.value}
+      /> */}
       </MapContainer>
     </div>
-  )
-}
+  );
+};
 
 export default FoodInsecurity
