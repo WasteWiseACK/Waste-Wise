f2ad5313f88bdb02af0fce3f0c9f249dc0a50800
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-10-24 14:47:18 -0400

    Added in more text to Home Page and added connection description to Impact Page

--- frontend/src/pages/Impact.jsx
+++ frontend/src/pages/Impact.jsx
@@ -6,6 +6,11 @@ const Impact = () => {
     <div className='Maps'>
       <FoodInsecurity />
       <C02 />
+      <p className="Description">Food waste and CO2 emissions are closely related in a number of ways.
+        Methane, one of the most potent greenhouse gasses, is released when food waste decomposes anaerobically in landfills.
+        Furthermore, when food is thrown away, the resources utilized to produce it—such as energy, land, and water—are wasted, which causes needless CO2 emissions from transportation and agriculture.
+        Additionally, by decreasing carbon sequestration, the conversion of forests into agricultural land for excessive food production exacerbates climate change.
+        Overall, as food waste minimizes the environmental effect of the food supply chain and reduces resource inefficiencies, it is essential for reducing CO2 emissions and building more sustainable food systems.</p>
     </div>
   )
 }
